46740deb76a42d5303288b673cb072e09b63c352
Wilson Romero <wilson.romero@gmail.com>
2020-02-15 11:03:58 -0500

    Solucion Reto

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
+import re
+from functools import reduce
 # Resolve the problem!!
 
 
 def run():
     # Start coding here
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        pattern = '[a-z]+'
+        mapped = map(lambda line: re.findall(pattern, line), f.readlines())
+        characters = reduce((lambda string, character: character), mapped)
+        result = ''.join(characters)
+        print(result)
 
 
 if __name__ == '__main__':
